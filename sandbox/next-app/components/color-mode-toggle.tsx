4cb37ba9d0a137c05b2eeb33f6b4701e52e02b09
Code Region <48864014+Challenge-Guy@users.noreply.github.com>
2025-03-26 15:42:42 +0100

    Update color-mode-toggle.tsx

--- sandbox/next-app/components/color-mode-toggle.tsx
+++ sandbox/next-app/components/color-mode-toggle.tsx
@@ -5,10 +5,10 @@ import { useTheme } from "next-themes"
 import { LuMoon, LuSun } from "react-icons/lu"
 
 export function ColorModeToggle() {
-  const { theme, setTheme } = useTheme()
-  const toggleColorMode = () => {
-    setTheme(theme === "light" ? "dark" : "light")
-  }
+  const { theme, setTheme } = useTheme() 
+  const toggleColorMode = () => { 
+    setTheme(theme === "light" ? "dark" : "light") 
+  } 
   return (
     <IconButton aria-label="toggle color mode" onClick={toggleColorMode}>
       {theme === "light" ? <LuMoon /> : <LuSun />}
